9308c7ee9da251071e5966fb4e7e0e6034d01daf
Michal <michalborowiec@op.pl>
2021-04-18 21:08:57 +0200

    advieces and quiz models add

--- driver_app/models.py
+++ driver_app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+
+class Advice(models.Model):
+    title = models.CharField(max_length=128)
+    photo_movie = models.FileField(upload_to='driver_app/uploads')
+    tags = models.ManyToManyField('Tag')
+    training = models.ForeignKey('Training', on_delete=models.SET_NULL, null=True)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Training(models.Model):
+    question = models.ManyToManyField('Question')
+
+
+class Question(models.Model):
+    content = models.TextField()
+    training = models.ForeignKey(Training, on_delete=models.CASCADE)
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    content = models.TextField()
+    correct = models.BooleanField(default=False)
+
